1c8af163292533e7109c976eb0966764ee4dec08
consagrohades-a11y <consagrohades@gmail.com>
2026-07-14 15:20:47 +0200

    Allow assigned practice to hide duplicate trainer nav

--- src/components/TrainerLayout.jsx
+++ src/components/TrainerLayout.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import TrainerNav from './TrainerNav';
 
-export default function TrainerLayout({ children, className = '' }) {
+export default function TrainerLayout({ children, className = '', showNav = true }) {
   return (
     <section className={`section-shell pb-10 pt-4 sm:pt-6 lg:pb-16 lg:pt-8 ${className}`}>
       <div className="mx-auto max-w-7xl">
-        <TrainerNav />
-        <div className="mt-5 sm:mt-8">{children}</div>
+        {showNav ? <TrainerNav /> : null}
+        <div className={showNav ? 'mt-5 sm:mt-8' : ''}>{children}</div>
       </div>
     </section>
   );
