e1558e540ab3492e1a14435a3c43a817506de545
consagrohades-a11y <consagrohades@gmail.com>
2026-08-11 11:13:01 +0200

    Use editorial learner canvas for all trainer pages

--- src/components/TrainerLayout.jsx
+++ src/components/TrainerLayout.jsx
@@ -3,11 +3,13 @@ import TrainerNav from './TrainerNav';
 
 export default function TrainerLayout({ children, className = '', showNav = true }) {
   return (
-    <section className={`section-shell pb-10 pt-4 sm:pt-6 lg:pb-16 lg:pt-8 ${className}`}>
-      <div className="mx-auto max-w-7xl">
-        {showNav ? <TrainerNav /> : null}
-        <div className={showNav ? 'mt-5 sm:mt-8' : ''}>{children}</div>
-      </div>
-    </section>
+    <div className="learner-editorial sblocco-trainer-canvas">
+      <section className={`section-shell pb-10 pt-4 sm:pt-6 lg:pb-16 lg:pt-8 ${className}`}>
+        <div className="mx-auto max-w-7xl">
+          {showNav ? <TrainerNav /> : null}
+          <div className={showNav ? 'mt-5 sm:mt-8' : ''}>{children}</div>
+        </div>
+      </section>
+    </div>
   );
 }
