5bca43f8ddac26456dfd08e4f806d0f273833dd3
consagrohades-a11y <consagrohades@gmail.com>
2026-09-22 01:23:45 +0200

    Remove retired legacy surface GeneralExpressionTrainer.jsx

--- src/pages/GeneralExpressionTrainer.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import DatabaseSrsTrainerPage from '../components/DatabaseSrsTrainerPage.jsx';
-
-export default function GeneralExpressionTrainer() {
-  return <DatabaseSrsTrainerPage trainerId="general-expression" seoTitle="General Expression Trainer | Sblocco Inglese" seoDescription="Espressioni quotidiane pubblicate, ripasso SRS e percorso guidato per gli studenti privati." />;
-}
