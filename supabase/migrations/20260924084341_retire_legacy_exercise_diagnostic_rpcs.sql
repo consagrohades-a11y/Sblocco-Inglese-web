64370fb2ed23836867b059fb3e218b4828b4430e
consagrohades-a11y <consagrohades@gmail.com>
2026-09-24 10:43:52 +0200

    Retire legacy diagnostic RPCs

---
+++ supabase/migrations/20260924084341_retire_legacy_exercise_diagnostic_rpcs.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.admin_rebuild_exercise_builder_diagnostics(uuid) from authenticated;
+revoke execute on function public.get_exercise_builder_learner_diagnostics(uuid) from authenticated;
